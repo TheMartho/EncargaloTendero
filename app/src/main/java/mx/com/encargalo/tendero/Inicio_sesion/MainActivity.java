6e4ac914b608698a3e014b4a521762286e60a729
TheMartho <martinpolancosaavedra@gmail.com>
2023-04-10 18:44:05 -0400

    first commit

---
+++ app/src/main/java/mx/com/encargalo/tendero/Inicio_sesion/MainActivity.java
@@ -0,0 +1,77 @@
+package mx.com.encargalo.tendero.Inicio_sesion;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.Menu;
+
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import mx.com.encargalo.R;
+import mx.com.encargalo.tendero.Util.Util;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration mAppBarConfiguration;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+
+        //TEMPORAL
+//        SharedPreferences sharedPreferences =
+//                getSharedPreferences(Util.SHAREDPREFERENCES, MODE_PRIVATE);
+//        SharedPreferences.Editor editor = sharedPreferences.edit();
+//        editor.putString(Util.VARGOB_ID_TENDERO, "1");
+//        editor.apply();
+
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_inicio,
+                R.id.rt_frgtiendaelegida,
+                R.id.nav_mi_tienda,
+                R.id.nav_mis_productos,
+                R.id.nav_mis_ordenes,
+                R.id.nav_mi_publicidad,
+                R.id.nav_mi_aprendizaje,
+                R.id.nav_soporte)
+                .setDrawerLayout(drawer)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+
+
+
+
+//        SharedPreferences creduser=getSharedPreferences("Credencial_Global_usuario",0);
+//        SharedPreferences.Editor editocred=creduser.edit();
+//        editocred.putString("CredId","73769972");
+//        editocred.commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+}
